99fb87d288bb60628ad578fb2c72396179be23c9
Miina Pulkkinen <miina.pulkkinen@gmail.com>
2019-05-04 20:45:36 +0300

    Second commit

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <div>
           <p>
-            Tästä rakentuu moottoripyörän oma päiväkirja - Bikery
+            Tästä rakentuu moottoripyörän oma päiväkirja - Bikery. Päivitetty.
           </p>
           </div>
           <a
