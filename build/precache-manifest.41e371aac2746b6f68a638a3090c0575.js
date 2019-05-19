c5872c7a27bf5bf5eb8f6a450ca3e53a0b8b7a3d
pulmii <miina.pulkkinen@gmail.com>
2019-05-19 20:17:38 +0300

    Update

---
+++ build/precache-manifest.41e371aac2746b6f68a638a3090c0575.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "96b9479be2a8d3db4388",
+    "url": "/static/js/main.58ce2f78.chunk.js"
+  },
+  {
+    "revision": "f468451e34ebc786ff2b",
+    "url": "/static/js/2.d9373093.chunk.js"
+  },
+  {
+    "revision": "96b9479be2a8d3db4388",
+    "url": "/static/css/main.edba6ef4.chunk.css"
+  },
+  {
+    "revision": "8063e06ce413835598bf90a8116293b6",
+    "url": "/index.html"
+  }
+];
